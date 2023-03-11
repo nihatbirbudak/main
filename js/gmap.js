42d64d22b23699d81c7f812db913d0d8a3bfa6fc
Nihat Birbudak <nihatbirbudak.94@hotmail.com>
2023-03-11 16:00:24 +0300

    yeni sayfa eklendi

---
+++ js/gmap.js
@@ -0,0 +1,18 @@
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 38.382367, lng: 27.132500 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
+  
+  window.initMap = initMap;
+  
---
+++ js/views.js
@@ -0,0 +1,8 @@
+
+$(function () {
+    var includes = $('[data-include]')
+    $.each(includes, function () {
+        var file = 'views/' + $(this).data('include') + '.html'
+        $(this).load(file)
+    })
+})
